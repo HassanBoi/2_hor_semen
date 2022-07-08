ab9705e67bd202543bcf98e2d2eab06afedb60d3
HassanBoi <108951456+HassanBoi@users.noreply.github.com>
2022-07-09 00:10:20 +0530

    Create Java_dev_GodMode.java
    Just shifting on a basic level

---
+++ Java_dev_GodMode.java
@@ -0,0 +1,10 @@
+package test;
+public class practice_ne {
+    public static int bitwise() {
+        int i=4;
+        return i>>>2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(bitwise());
+    }
